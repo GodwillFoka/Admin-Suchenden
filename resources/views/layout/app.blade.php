c92f0544c5847ed9b2fd623d03648a8b1696f0b2
FOKA GODWILL <fokagodwill@gmail.com>
2023-01-04 02:12:21 +0100

    Initialisation vercel

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Institut Die Suchenden</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="shortcut icon" href="{{ asset('Images/Institut_die_suchenden.png') }}" type="image/x-icon">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
@@ -59,6 +60,9 @@
     <!-- jQuery Mapael -->
     <script src="{{ asset('Style/plugins/jquery-mousewheel/jquery.mousewheel.js') }}"></script>
     <script src="{{ asset('Style/plugins/raphael/raphael.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     <script src="{{ asset('Style/plugins/jquery-mapael/jquery.mapael.min.js') }}"></script>
     <script src="{{ asset('Style/plugins/jquery-mapael/maps/usa_states.min.js') }}"></script>
     <!-- ChartJS -->
